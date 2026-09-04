70fa4f5699876266b12cacf070777f162bea878d
d224407 <nguynlanh63@gmail.com>
2026-09-04 20:44:48 +0700

    Remove Kotlin Android plugin from build.gradle.kts
    Removed Kotlin Android plugin version 2.4.10 from plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "9.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
     id("com.google.dagger.hilt.android") version "2.60.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     kotlin("plugin.serialization") version "2.1.0" apply false
