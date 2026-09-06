5c5c54e00e7766f1cbfd431f2342eb161f4131d3
d224407 <nguynlanh63@gmail.com>
2026-09-06 10:43:55 +0700

    fix plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -138,7 +138,7 @@ android {
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.19.0")
+  implementation("androidx.core:core-ktx:1.18.0")
   // Compose BOM
   val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
   implementation(composeBom)
