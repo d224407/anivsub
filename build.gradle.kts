954c98d71628d49ed979cfd19e738e3bd9f1580a
d224407 <nguynlanh63@gmail.com>
2026-09-06 10:31:59 +0700

    Update Kotlin and KSP plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ plugins {
     id("com.android.application") version "8.8.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.10" apply false
     id("com.google.dagger.hilt.android") version "2.54" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    kotlin("plugin.serialization") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
+    kotlin("plugin.serialization") version "2.3.10" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
